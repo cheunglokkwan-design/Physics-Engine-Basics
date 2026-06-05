696839f60a91fbc734cb2ceabdfb17b6428e9e20
Cheung Lok Kwan <cheunglokkwan@gmail.com>
2026-06-06 00:34:57 +0800

    Update physicsEngine.py

--- physicsEngine.py
+++ physicsEngine.py
@@ -12,3 +12,23 @@ class Vector2D:
     
     def multiply(self, scalar:float) -> 'Vector2D':
         return Vector2D(self.x * scalar, self.y * scalar)
+
+
+class Particle: 
+    def __init__(self, mass:float, position:Vector2D, velocity:Vector2D):
+        self.mass = mass
+        self.position = position
+        self.velocity = velocity
+        self.forces = Vector2D(0.0,0.0)
+
+    def apply_force(self, force:Vector2D):
+        self.forces = self.forces.add(force)
+    
+    def step(self, dt:float):
+        acceleration = self.forces.multiply(1.0/self.mass)
+
+        self.velocity = self.velocity.add(acceleration.multiply(dt))
+
+        self.position = self.position.add(self.velocity.multiply(dt))
+
+        self.forces = Vector2D(0.0,0.0)
