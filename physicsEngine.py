b0d8139bbf797dee1408df3e66eaec71b56e6d76
Cheung Lok Kwan <cheunglokkwan@gmail.com>
2026-06-07 18:09:17 +0800

    Update physicsEngine.py

--- physicsEngine.py
+++ physicsEngine.py
@@ -7,8 +7,8 @@ class Vector2D:
         self.x = x
         self.y = y
 
-    def add(self, other:'Vector2D')->Vector2D:
-        return Vector2D(self + other.x, self.y + other.y)
+    def add(self, other:'Vector2D')->'Vector2D':
+        return Vector2D(self.x + other.x, self.y + other.y)
     
     def multiply(self, scalar:float) -> 'Vector2D':
         return Vector2D(self.x * scalar, self.y * scalar)
@@ -32,3 +32,40 @@ class Particle:
         self.position = self.position.add(self.velocity.multiply(dt))
 
         self.forces = Vector2D(0.0,0.0)
+
+class SimulationEngine:
+    def __init__(self, gravity_y: float = -9.81):
+        self.gravity = Vector2D(0.0, gravity_y)
+        self.objects: List[Particle] = []
+        
+    def spawn_object(self, obj: Particle):
+        self.objects.append(obj)
+
+    def run(self, duration: float, dt: float):
+        current_time = 0.0
+
+        print(f"{'Time (s)':<10} | {'Pos X (m)':<12} | {'Pos Y (m)':<12} | {'Vel Y (m/s)':<12}")
+        print("-" * 55)
+
+        while current_time <= duration:
+            for obj in self.objects:
+                gravitational_force = self.gravity.multiply(obj.mass)
+                obj.apply_force(gravitational_force)
+
+                obj.step(dt)
+                
+                if round(current_time / 0.2, 5).is_integer():
+                    print(f"{current_time:<10.2f} | {obj.position.x:<12.3f} | {obj.position.y:<12.3f} | {obj.velocity.y:<12.3f}")
+            current_time += dt
+
+if __name__ == "__main__":
+    sim = SimulationEngine(gravity_y = -9.81)
+
+    initial_pos = Vector2D(x=0.0,y = 0.0)
+    initial_vel = Vector2D(x = 5.0, y = 15.0)
+    projectile = Particle(mass=0.5, position=initial_pos, velocity=initial_vel)
+
+    sim.spawn_object(projectile)
+    
+
+    sim.run(duration=3.0, dt=0.01)
